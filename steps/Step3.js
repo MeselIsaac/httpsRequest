7aec1b88001171c34a3889f8795a1b718cff108c
Mesel <meselisaac03@hotmail.com>
2019-03-11 16:41:27 +0000

    Completed Step3

---
+++ steps/Step3.js
@@ -0,0 +1,24 @@
+var https = require('https');
+
+function getAndPrintHTML (options) {
+  var buffer = ''
+  https.get(options, function (response) {
+    response.setEncoding('utf8');
+    response.on('data', function (data) {
+      buffer += data;
+    })
+
+    response.on('end', function() {
+      console.log(buffer);
+    })
+  })
+
+}
+
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step3.html'
+};
+
+
+getAndPrintHTML(requestOptions);
